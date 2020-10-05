5ae5d41f4d3af8b428f9965b2a24c6c54902cb4f
Jordan Rivers <j.russell.rivers@gmail.com>
2020-10-05 14:46:16 -0400

    preliminary search function

--- src/Components/MyInspections.js
+++ src/Components/MyInspections.js
@@ -1,12 +1,36 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 const MyInspections = () => {
 
+    const [photos,setPhotos] = useState()
+    const [searchTerm,setSearchTerm] = useState("")
+    const [searchResults, setSearchResults] = useState([]);
+
+    const onChange = text => {
+        setSearchTerm(text)
+    }
+
+    useEffect(() => {
+        fetch('/grabPhotos')
+        .then(r=>r.json())
+        .then(data=>setPhotos(data))
+    },[])
+
+    
+    let results
+    if(searchTerm){
+        results = photos.filter(photo =>
+            photo.file_name.toLowerCase().includes(searchTerm)
+        );
+    }
+    setSearchResults(results);
+
+
     return(<div>
         <h1 className="title"> Your Inspections</h1>
         <ul>
-            <li> - UseEffect to Fetch Inspections</li>
+            <li> Map Search Results Here</li>
             <li>  - Ability to click on each inspection, fetch images</li>
 
         </ul>
